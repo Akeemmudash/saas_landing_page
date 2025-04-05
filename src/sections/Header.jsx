90a78755c33a93c273710a9a97748a9b24d4ea14
Akeem Mudashiru <akeemmudash@gmail.com>
2025-04-05 17:23:18 +0100

    feat: Added animation and background to navbar on scroll

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,23 +1,48 @@
 import clsx from "clsx";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
+import { cn } from "../utils";
 
-const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-primary-4 uppercase transition-colors duration-500 cursor-pointer hover:text-primary-1 max-lg:my-4 max-lg:h5">
-    {title}
-  </LinkScroll>
-);
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={setIsOpen(false)}
+      to={title}
+      offset={-100}
+      activeClass="nav-active"
+      spy
+      smooth
+      className="base-bold text-primary-4 uppercase transition-colors duration-500 cursor-pointer hover:text-primary-1 max-lg:my-4 max-lg:h5"
+    >
+      {title}
+    </LinkScroll>
+  );
   return (
-    <header className="fixed top-0 left-0 w-full py-10 z-50">
+    <header
+      className={cn(
+        "fixed top-0 left-0 w-full py-10 z-50 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a href="" className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
 
         <div
-          className={clsx(
+          className={cn(
             "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-secondary-2 max-lg:opacity-0",
             isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
           )}
@@ -33,11 +58,11 @@ const Header = () => {
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
-                    className={clsx(
-                      "max-lg:hidden transition-transform duration-500"
+                    className={cn(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
                     )}
                   >
                     <img
